e813280d235e3577035be5f6711a7b505faddcbd
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-30 11:21:54 +0200

    Modif PHP Correction 2

---
+++ PHP2_Correction_2.php
@@ -0,0 +1,19 @@
+
+<table border="1"> 
+    <head>
+        <tr>Pays</tr>
+        <tr>Capitales</tr>
+    </head>
+</table>
+
+<?php
+$capitales=array(
+    "France"=>"Paris",
+    "Allemagne"=>"Berlin",
+    "USA"=>"Washington",
+    "Italie"=>"Rome");
+
+    function afficherTableauHTML($capitales){
+
+    }
+?>
