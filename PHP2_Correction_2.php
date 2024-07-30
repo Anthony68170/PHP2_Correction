b7ebe42882e561df62294dbab36222d7920b6e4c
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-30 11:40:14 +0200

    Modif PHP2 COrrection Ex2

--- PHP2_Correction_2.php
+++ PHP2_Correction_2.php
@@ -14,9 +14,12 @@
         </tr>
         
         <tr>
-            <td>France</td><td>Paris</td>
             <td>Allemagne</td><td>Berlin</td>
+        </tr>
+        <tr>
             <td>USA</td><td>Washington</td>
+        </tr>
+        <tr>
             <td>Italie</td><td>Rome</td>
         </tr>
     </tbody>
