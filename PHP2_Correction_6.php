acedc8b74a5e4b6ad881a264fee11d5bbd84cb98
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-31 15:09:48 +0200

    Modif Correction PHP2 Exo 6

--- PHP2_Correction_6.php
+++ PHP2_Correction_6.php
@@ -5,28 +5,30 @@
 </head>
 
 <body>
-<>  <form id="listderoule" name="listderoule" method="post"> 
+    <form id="listderoule" name="listderoule" method="post"> 
         <select>  
   <option value="Select">Select</option>
   <option value="Monsieur">Monsieur</option>  
   <option value="Madame">Madame</option> 
   <option value="Mademoiselle">Mademoiselle</option> 
 
-<?php
-
-$elements = array(
-    "Monsieur",
-    "Madame",
-    "Mademoiselle"
-);
-    foreach($elements as $element){
-    echo"<form>";
-    echo "<option value> ".$elements." ".$element.">";
-    echo "</form>";
-    
-    echo alimenterListeDeroulante($elements);
+        <?php
+        $elements = array(
+        "Monsieur",
+        "Madame",
+        "Mademoiselle"
+    );
+    function alimenterListeDeroulante { 
+        foreach($elements as $element){
+        echo"<form>";
+        echo "<option value> ".$elements." ".$element." </option>";
+        echo "</form>";
+        
+        echo alimenterListeDeroulante($elements);
         }
+    }
         ?>
+
         </select>
     </form>
 </body>
