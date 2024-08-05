5733f56fe7e42e6cecec5836cc4eb30aaac69759
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-08-05 10:05:32 +0200

    Modif Correction PHP 2 Exo 8

--- PHP2_Correction_8.php
+++ PHP2_Correction_8.php
@@ -2,25 +2,28 @@
 
 <?php
 
-$image :"http://my.mobirise.com/data/userpic/764.jpg";
-$repeter = 4;
+$url ="http://my.mobirise.com/data/userpic/764.jpg";
 
-function repeterImage($image, $repeter) {
-    for ($i = 0; $i < $repeter; $i++)
-};
-
-echo repeterImage();
+function afficherImage($url) {
+    echo "<img src='<?= $url ?>'>";
+}
 
 
+?>
 
+// $repeter = 4;
 
+// function repeterImage($url, $repeter) {
+//     for ($i = 0; $i < $repeter; $i++)
+// };
+
+// echo "<img src="<?=url??>">";
 
 
 // $function repeterImage ($url,$nbRep){
-//     foreach ($nbRep as $key => $value) {
-//         if ($value["$repeter"] == $url) {
-// }
-//     }
-// echo "".$value[""]."".$value[""].
-?>
+    //     foreach ($nbRep as $key => $value) {
+        //         if ($value["$repeter"] == $url) {
+            // }
+            //     }
+            // echo "".$value[""]."".$value[""].
 
