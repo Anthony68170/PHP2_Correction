8bf0aec990d929b621f157c3f6efde82fcb21761
ANTHO_XB <anthony_xb@proton.me>
2024-08-19 16:59:16 +0200

    Correction_Exo_12 PHP2

---
+++ PHP2_correction_12.php
@@ -0,0 +1,46 @@
+<?php
+
+// $tableauValeurs=array(true,"texte",10,25.369,array("valeur1","valeur2"));
+
+// function afficherTableau($tableauValeurs){
+//     // foreach($tableauValeurs as $value) {
+//     foreach ($tableauValeurs as $value) {
+//     echo gettype($value), "\n";
+        
+//         // var_dump(gettype($value) . " " . :string) ;
+//     }
+
+// };
+
+// afficherTableau($tableauValeurs);
+
+
+?>
+
+
+<?php
+
+// $tableauValeurs = array(
+//     true,
+//     "texte",
+//     10,
+//     25.369,
+//     array("valeur1","valeur2"),
+// );
+
+// foreach ($tableauValeurs as $values) {
+//     echo var_dump($values), "\n";
+// }
+
+?>
+
+<?php
+$tableauValeurs = array(
+    true,
+    "texte",
+    10,
+    25.369,
+    array("valeur1","valeur2"),
+);
+var_dump($tableauValeurs);
+?>
