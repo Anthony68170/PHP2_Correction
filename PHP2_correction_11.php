bc614ebd6dad2a13cce047379d200d3d64fc6db7
ANTHO_XB <anthony_xb@proton.me>
2024-08-19 16:03:45 +0200

    modif Correction_Exo 11 PHP2

--- PHP2_correction_11.php
+++ PHP2_correction_11.php
@@ -1,12 +1,33 @@
 
+<!-- https://www.php.net/manual/fr/intldateformatter.format.php -->
+
 <?php
-$date = new DateTime ("d/m/y");
-function formaterDateFr("2018-02-23"){
-    $datefr=date_format($date);
-    $dateText=
-    return $dateText;
+function formaterDateFr($date_str) {
+    // Créer un objet DateTime à partir de la chaîne de date
+    $date_obj = DateTime::createFromFormat('Y-m-d', $date_str);
+ 
+    // Vérifier si la date est valide
+    if ($date_obj === false) {
+        return "Date invalide";
+    }
+ 
+    // Créer un formateur de date pour la locale française
+    $formatter = new IntlDateFormatter(
+        'fr_FR',
+        IntlDateFormatter::FULL,
+        IntlDateFormatter::NONE,
+        'Europe/Paris',
+        IntlDateFormatter::GREGORIAN
+    );
+ 
+    // Formater la date
+    $date_fr = $formatter->format($date_obj);
+ 
+    // Retourner la date formatée
+    return $date_fr;
 }
-
-
-echo formaterDateFr($date);
+ 
+// Afficher la date formatée
+echo formaterDateFr("2018-02-23");
 ?>
+ 
