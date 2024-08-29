fa48dc2e1544406cb2ba681ec865c26d802e7242
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-08-29 14:47:11 +0200

    Exo13 PHP2 POO CLASS ATTRIBUT HYDRAT(boucle)

--- PHP2_Correction_Ex13_POO.php
+++ PHP2_Correction_Ex13_POO.php
@@ -7,142 +7,138 @@
 <?php
 // Créer l'objet principal avec ces attributs 
 class Voiture {// definir la classe
-    private $marque;// propriétés privées
-    private $modele;
-    private $nbPortes;
+    private $_marque;// propriétés privées
+    private $_modele;
+    private $_nbPortes;
 
     // private $vitesseActuelle=0;
-    protected $vitesseActuelle;
+    private $_vitesseActuelle;
 
 // Méthode magique => __ (2x underscore)
-    public function __construct($valeurs = array()) { 
+    public function __construct($valeurs = array()) 
+    { 
         $this->_vitesseActuelle = 0; // Reset la vitesse actuelle à 0 par défaut
-        if (!empty($valeurs)) {
+        if (!empty($valeurs)) 
+        {
             $this->hydrate($valeurs);
         }
     }
 
-    // Méthode pour hydrater l'objet
-    public function hydrate(array $donnees) {
-        foreach ($donnees as $attribut => $valeur) {
+    // Méthode avec une boucle foreach exploitant l'hydratation d'un l'objet
+    public function hydrate(array $donnees) 
+    {
+        foreach ($donnees as $attribut => $valeur) 
+        {
             $methode = 'set' . str_replace(' ', '', ucwords(str_replace('_', ' ', $attribut)));
-            if (is_callable(array($this, $methode))) {
+            if (is_callable(array($this, $methode))) 
+            {
                 $this->$methode($valeur);
             }
         }
     }
 
-    
 // placer les SET  
-public function setMarque($_marque){
-    $this->_marque = $_marque;
+public function setMarque($marque)
+{
+    $this->_marque = $marque;
 }
 
-public function setModele($modele){
+public function setModele($modele)
+{
     $this->_modele = $modele;
 }
 
-public function setNbPortes($_nbPortes){
-    $this->_marque = $_nbPortes;
+public function setNbPortes($nbPortes)
+{
+    $this->_nbPortes = $nbPortes;
 }
 
-public Function setvitesseActuelle($vitesse){
+public Function setvitesseActuelle($vitesse)
+{
     $this->vitesseActuelle = $vitesse;
 }
 
 //Placer les GET
-public function getMarque(){
+public function getMarque()
+{
     return $this->_marque;
 }
     
-public function getModele(){
+public function getModele()
+{
     return $this->_modele;
 }
 
-public function getNbPortes(){
+public function getNbPortes()
+{
     return $this->_nbPortes;
 }
 
 public Function getvitesseActuelle(){
-    return $this->vitesseActuelle;
+    return $this->_vitesseActuelle;
 }
 
 //Placer les METHODES / Ce sont les actions à citer PUIS ACTIVER SUR UNE CONDITION
-public function demarrer(){
-    echo  "le véhicule " .$this->_marque . " " . $this->_modele . "démarre.";
+public function demarrer()
+{
+    echo  "le véhicule " .$this->_marque . " " . $this->_modele . " démarre.";
 }
 
-public function accelerer(){
-    $this->vitesseActuelle +=$vitesse;
-    echo "<br />Le véhicule " .$this->_marque . " " . $this->_modele . "accélère de " .$vitesse. " km/h. ";
+public function accelerer($vitesse)
+{
+    $this->_vitesseActuelle += $vitesse;
+    echo "<br>Le véhicule " .$this->_marque . " " . $this->_modele . " accélère de " .$vitesse. " km/h. ";
 }
-
-public function stopper(){
+public function stopper()
+{
     $this->vitesseActuelle = 0;
-    echo "Le véhicule " .$this->_marque . " " . $this->_modele . "est à l'arrêt " .$vitesse. " km/h. ";
+    echo "<br>Le véhicule " .$this->_marque . " " . $this->_modele . " est à l'arrêt ";
 }
 
-public function ralentir(){
-    $this->vitesseActuelle -=$vitesse;
-    if (_vitesseActuelle){
-        
-    }
-    echo "Le véhicule " .$this->_marque . " " . $this->_modele . "ralentit de " .$vitesse. " km/h. ";
-}
-
-// public function ralentir(){
-//     $this-> $vitesse =0;
-//     if $vitesse < 0;
-// }
-// echo "le véhicule a réduit sa vitesse à "
-'
 // NOUVELLE METHODE POUR RALENTIR
-public function ralentir(){
-    $this->vitesseActuelle -=$vitesse;
-    if $this->_vitesseActuelle <$vitesse;
-    echo "Le véhicule " .$this->_marque . " " . $this->_modele . "ralenti de " .$vitesse. " km/h. ";
+public function ralentir($vitesse){
+    // si la vitesse de décélération est inférieure à la vitesse actuelle, et que la vitesse actuelle alors je décélère 
+    if ($this->_vitesseActuelle>$vitesse){
+        $this->_vitesseActuelle -=$vitesse;
+        echo "<br>Le véhicule " .$this->_marque . " " . $this->_modele . "ralenti de " .$vitesse. " km/h. ";
+    }
+    else echo "<br>Le vitesse de décélration est trop importante ";
 }
 
-
-//AFFICHER AVEC LES METHODES
-public function affichagePersVehicule(){
-    echo "Nom et modèle du véhicule : " .$this->_marque .  " . $this->_modele .";
-    echo "La vitesse actuelle est de : " .$this_>_vitesseActuelle . " km/h";
+// AFFICHER AVEC LES METHODES
+public function affichagePersVehicule()
+{
+    echo "<br>La vitesse actuelle est de : " .$this->_vitesseActuelle . " km/h";
 }
-
+}
 
 // Créer les élements à obtenir
-$peugeot408 = new Voiture(array(
+$peugeot408 = new Voiture(array
+(
 '_marque' => 'Peugeot',
 '_modele' => '408',
-'_nbPortes' => 5)
+'_nbPortes' => 5
+)
 );
-$citroenC4 = new Voiture(array(
+$citroenC4 = new Voiture(array
+(
 '_marque' => 'Citroen',
 '_modele' => 'C4',
-'_nbPortes' => 3)
+'_nbPortes' => 3
+)
 );
 
 //UTILISER LES OBJETS/Fonctions 
 $peugeot408->demarrer();
 $peugeot408->accelerer(50);
 $peugeot408->affichagePersVehicule();
-$peugeot408->ralentir(10); //Bonus option ralentir
 
+//Bonus option ralentir
+$peugeot408->ralentir(10);
 
 $citroenC4->demarrer();
 $citroenC4->stopper();
 $citroenC4->accelerer(20);
 
-}
-
-
-// public function setMarque($marque){
-//     $this->_marque = $marque;
-//     }
-//     public function setCpuClock($speed){
-//     $this->_cpuClock = $speed;
-//     }
-//     …
-
+?>
 
