6c726acb223e8feed63ee96d218eab05261664e0
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-08-30 10:28:57 +0200

    Exo 14 PHP2 POO _HERITAGE et AJOUT de Class

--- Exo14 PHP2_POO CLASS ATTRIBUTS PROPRIETES HERITAGES.php
+++ Exo14 PHP2_POO CLASS ATTRIBUTS PROPRIETES HERITAGES.php
@@ -1,37 +1,42 @@
 <?php
 
-class V1
+class Voiture // 
 {
     private $_marque;
     private $modele;
-    private $nbPortes;
     public function __construct($marque, $modele, $nbPortes)
     {
         $this -> _marque = $marque;
         $this -> _modele = $modele;
-        $this -> _nbPortes = $nbPortes
+    }
+    public function getInos(){
+        return "Marque : " $this -> _marque . " Modèle : " . $this -> _modele .;
     }
 }
+class Ve1 extends V1
+{
+    private $_autonomie;
+    public function __construct($autonomie);
+    {
+        $this -> $_autonomie = $autonomie;
+        echo "n^hohjîoj p";
+        parent::v1();
+    }
+    
+}
+
+
+
 // Ajouter Get pour avoir les infos 
 //Placer les GET
 // public function getNbPortes()
 // {
 //     return $this->_nbPortes;
 // }
-public function getInfos()
-{
-    return $this->_infos;
-}
 
-class Ve1 extends V1
-{
-    private $_autonomie;
-    public function __construct($autonomie);
-    {
-echo "n^hohjîoj p";
-parent::v1();
-    }
-
-}
-$v1 = new Ve1();
-$v1->save();
+// public function getInfos()
+// {
+//     return $this->_infos;
+// }
+// $v1 = new Ve1();
+// $v1->save();
